6efd4d4bfdab698b0f72277a70a352cfce74a287
meanish <anishgshrestha@gmail.com>
2025-04-22 16:30:19 +0545

    added storage Data values

--- src/electron/resourceManager.js
+++ src/electron/resourceManager.js
@@ -1,11 +1,13 @@
-import osUtils from 'os-utils'
+import osUtils from 'os-utils';
+import fs from "fs"
 const Polling_Interval = 500;
 
 export function pollrosource() {
     setInterval(async () => {
         const cpuUsage = await getCpuUsage()
         const getRamUsage = getramUsage()
-        console.log({ cpuUsage, getRamUsage })
+        const storageData = getStorageData()
+        console.log({ cpuUsage, getRamUsage, storageData })
     }, Polling_Interval)
 }
 
@@ -20,3 +22,15 @@ function getCpuUsage() {
 function getramUsage() {
     return 1 - osUtils.freememPercentage();
 }
+
+/* getStorage Data */
+function getStorageData() {
+    const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/'); //get rootfile system basically gives whoe disk space 
+    const total = stats.bsize * stats.blocks; //size of total block how many bites my disks have
+    const free = stats.bsize * stats.bfree; //how much space is free
+    return {
+        total: Math.floor(total / 1_000_000_000), //toal space by gigbyte kilobye mb byte
+        usage: 1 - free / total //used storage
+    }
+}
+
