c4aab925d726542b5a232095b362ee9fe03c3987
meanish <anishgshrestha@gmail.com>
2025-04-22 16:14:02 +0545

    added ram usage

--- src/electron/resourceManager.js
+++ src/electron/resourceManager.js
@@ -2,10 +2,21 @@ import osUtils from 'os-utils'
 const Polling_Interval = 500;
 
 export function pollrosource() {
-    setInterval(() => { getCpuUsage() }, Polling_Interval)
+    setInterval(async () => {
+        const cpuUsage = await getCpuUsage()
+        const getRamUsage = getramUsage()
+        console.log({ cpuUsage, getRamUsage })
+    }, Polling_Interval)
 }
 
-
+/* get used cpu usage */
 function getCpuUsage() {
-    osUtils.cpuUsage((percentage) => console.log(percentage))
+    return new Promise(resolve => {
+        osUtils.cpuUsage(resolve)
+    })
+}
+
+/* get used ram usage */
+function getramUsage() {
+    return 1 - osUtils.freememPercentage();
 }
