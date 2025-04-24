5f0def7fa17f972d27d509fe2b383fa70e83aa3c
meanish <anishgshrestha@gmail.com>
2025-04-24 16:29:09 +0545

    tray stays even electron closes

--- src/electron/main.js
+++ src/electron/main.js
@@ -28,9 +28,50 @@ const createWindow = () => {
 
     // icon tray for ios and win
     new Tray(path.join(getIconPath(), process.platform === "darwin" ? "icons8(ios)" : "icons8(win).png"))
+
+
+    // prevent tray to diapear even after app closes
+    // win.on('close', (e) => {
+    //     e.preventDefault() //using this prevents to close the electron app even using X
+
+    // })
+    handleCloseEvents(win)
+
+    /* 
+    Q. How to actually close electron
+    a. all windows close => "before-event"-event => app stops
+    b. ""before-quit" event => all windows get closed => app stops
+    */
+
+    function handleCloseEvents(win) {
+        let willClose = false
+
+        if (willClose) { return }
+
+
+        win.on('close', (e) => {
+            e.preventDefault() //using this prevents to close the electron app even using X
+            win.hide()
+            // for mac
+            if (app.dock) {
+                app.dock.hide()
+            }
+
+
+        })
+
+
+        app.on('before-quit', () => {
+            willClose = true;
+        })
+
+        win.on('show', () => {
+            willClose = false;
+        })
+    }
 }
 
 
 app.whenReady().then(() => {
     createWindow()
-})
+}) 
