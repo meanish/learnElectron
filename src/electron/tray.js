df8bf422ec45eec84b38c479a484ab548289c572
meanish <anishgshrestha@gmail.com>
2025-04-25 13:55:11 +0545

    show tray function

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -6,15 +6,27 @@ import path from 'path'
 export function createTray(mainWindow) {
     const newTray = new Tray(path.join(getIconPath(), process.platform === "darwin" ? "icons8(ios).png" : "icons8(win).png"))
 
-    // add quit funct in tray
+    // add quit and show funct in tray icon
     const contextMenu = newTray.setContextMenu(
         Menu.buildFromTemplate([
+
+            {
+                label: 'Show',
+                click: () => {
+                    mainWindow.show();
+                    if (app.dock) {
+                        app.dock.show()
+                    }
+                }
+            },
             {
                 label: 'Quit',
                 click: () => {
                     app.quit();
-                }
-            }
+                },
+                // type: "checkbox"
+            },
+
         ])
     )
 
