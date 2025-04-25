37f6788ff92b543fbd7f6c48d60ea993f32abdaa
meanish <anishgshrestha@gmail.com>
2025-04-25 13:40:59 +0545

    added tooltipe and quit funct in tray

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -7,9 +7,19 @@ export function createTray(mainWindow) {
     const newTray = new Tray(path.join(getIconPath(), process.platform === "darwin" ? "icons8(ios).png" : "icons8(win).png"))
 
     // add quit funct in tray
-    newTray.setContextMenu(
-        Menu.buildFromTeplate([{
-            label: 'Quit',
-            click: () => app.quit(),
-        }]))
+    const contextMenu = newTray.setContextMenu(
+        Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: () => {
+                    app.quit();
+                }
+            }
+        ])
+    )
+
+    newTray.setToolTip('basicElectron');
+    newTray.setContextMenu(contextMenu);
+
+    return newTray;
 }
